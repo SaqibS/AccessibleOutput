b035a1ceac8d07d9661ecaa1a1ef7ea3a3679cef
Saqib Shaikh <me@saqibshaikh.com>
2015-05-09 09:46:34 -0700

    Updating assembly properties.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AccessibleOutput")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Accessible Output")]
+[assembly: AssemblyDescription("Accessible Output allows you to create programs which speak either using the deault Windows (SAPI) voice, or to speak using a screen reader's API (providing a more natural user experience).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AccessibleOutput")]
+[assembly: AssemblyCompany("Saqib Shaikh")]
+[assembly: AssemblyProduct("Accessible Output")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
