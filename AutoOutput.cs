8a3c7b0ad1eed158cf0e6ff51e60f394a27b37c1
Saqib Shaikh <me@saqibshaikh.com>
2015-05-08 22:05:03 -0700

    Adding SapiOutput and AutoOutput.

---
+++ AutoOutput.cs
@@ -0,0 +1,47 @@
+﻿namespace AccessibleOutput
+{
+    using System;
+    using System.Linq;
+
+    public class AutoOutput : IAccessibleOutput
+    {
+        private static readonly IAccessibleOutput[] outputs = { new JawsOutput(), new NvdaOutput(), new SapiOutput() };
+
+        public AutoOutput()
+        {
+        }
+
+        public bool IsAvailable()
+        {
+            return outputs.Any(x => x.IsAvailable());
+        }
+
+        public void Speak(string text)
+        {
+            this.Speak(text, false);
+        }
+
+        public void Speak(string text, bool interrupt)
+        {
+            IAccessibleOutput output = this.GetFirstAvailableOutput();
+
+            if (interrupt)
+            {
+                output.StopSpeaking();
+            }
+
+            output.Speak(text);
+        }
+
+        public void StopSpeaking()
+        {
+            IAccessibleOutput output = this.GetFirstAvailableOutput();
+            output.StopSpeaking();
+        }
+
+        private IAccessibleOutput GetFirstAvailableOutput()
+        {
+            return outputs.FirstOrDefault(x => x.IsAvailable());
+        }
+    }
+}
---
+++ SapiOutput.cs
@@ -0,0 +1,40 @@
+﻿namespace AccessibleOutput
+{
+    using System;
+    using System.Speech.Synthesis;
+
+    public class SapiOutput : IAccessibleOutput
+    {
+        private SpeechSynthesizer synth;
+
+        public SapiOutput()
+        {
+            this.synth = new SpeechSynthesizer();
+        }
+
+        public bool IsAvailable()
+        {
+            return true;
+        }
+
+        public void Speak(string text)
+        {
+            this.Speak(text, false);
+        }
+
+        public void Speak(string text, bool interrupt)
+        {
+            if (interrupt)
+            {
+                this.StopSpeaking();
+            }
+
+            this.synth.SpeakAsync(text);
+        }
+
+        public void StopSpeaking()
+        {
+            this.synth.SpeakAsyncCancelAll();
+        }
+    }
+}
